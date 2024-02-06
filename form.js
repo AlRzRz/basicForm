d8b2656198df52ca234ee3bb90634c9319b7f1d1
AR <alir858@yahoo.com>
2024-02-06 07:21:03 -0500

    Final tweaks and slight responsiveness up to 900px width

--- form.js
+++ form.js
@@ -1,5 +1,77 @@
-let passwordBlock = document.querySelector('.passwordinput');
-let confirmBlock = document.querySelector('.passwordconfirm');
+let passwordInput = document.querySelector('.passwordinput');
+let confirmInput = document.querySelector('.passwordconfirm');
 let errorStatement = document.querySelector('.noMatchMessage');
 
+// passwordInput.addEventListener('input', () => {
+//     if (passwordInput.value !== confirmInput.value) {
+//         confirmInput.setCustomValidity('Passwords do not match!');
+//         // passwordInput.setCustomValidity('Passwords do not match!');
+//         passwordInput.setAttribute('isvalid', 'false');
+//         errorStatement.style.display = 'block';
+//     } else {
+//         passwordInput.setCustomValidity('');
+//         confirmInput.setCustomValidity('');
+//         errorStatement.style.display = 'none';
+//     }
+// })
 
+// confirmInput.addEventListener('input', () => {
+//     if (passwordInput.value !== confirmInput.value) {
+//         confirmInput.setCustomValidity('Passwords do not match!');
+//         passwordInput.setCustomValidity('Passwords do not match!');
+//         errorStatement.style.display = 'block';
+//     } else {
+//         passwordInput.setCustomValidity('');
+//         confirmInput.setCustomValidity('');
+//         errorStatement.style.display = 'none';
+//     }
+// })
+
+confirmFocusCheck = false;
+
+
+confirmInput.addEventListener('input', () => {
+    confirmFocusCheck = true;
+})
+
+
+confirmInput.addEventListener('input', () => {
+    if (passwordInput.value !== confirmInput.value) {
+        confirmInput.setCustomValidity('Passwords do not match!');
+        errorStatement.style.display = 'block';
+        passwordInput.classList.add('error');
+        confirmInput.classList.add('error');
+    } else {
+        confirmInput.setCustomValidity('');
+        errorStatement.style.display = 'none';
+        
+        if (passwordInput.classList.contains('error')) {
+            passwordInput.classList.remove('error');
+        }
+        if (confirmInput.classList.contains('error')) {
+            confirmInput.classList.remove('error');
+        }
+    }
+})
+
+
+passwordInput.addEventListener('input', () => {
+    if (confirmFocusCheck) {
+        if (passwordInput.value !== confirmInput.value) {
+            confirmInput.setCustomValidity('Passwords do not match!');
+            errorStatement.style.display = 'block';
+            passwordInput.classList.add('error');
+            confirmInput.classList.add('error');
+        } else {
+            confirmInput.setCustomValidity('');
+            errorStatement.style.display = 'none';
+            
+            if (passwordInput.classList.contains('error')) {
+                passwordInput.classList.remove('error');
+            }
+            if (confirmInput.classList.contains('error')) {
+                confirmInput.classList.remove('error');
+            }
+        }
+    }
+})
